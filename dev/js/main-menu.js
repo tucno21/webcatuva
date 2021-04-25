8142d8d01bdc5446287d4b36eb5c1508c1be8973
tucno21 <carlitostucno@gmail.com>
2021-04-25 11:24:24 -0500

    add dark mode

--- dev/js/main-menu.js
+++ dev/js/main-menu.js
@@ -5,6 +5,8 @@ const nav__menu1 = document.getElementById('nav__menu1');
 const nav__menu2 = document.getElementById('nav__menu2');
 const nav__menu3 = document.getElementById('nav__menu3');
 const nav__menu4 = document.getElementById('nav__menu4');
+//======MODO ISCURO
+const btnSwitch = document.querySelector('#switch')
 
 toggle.onclick = function () {
     toggle.classList.toggle('activador');
@@ -26,4 +28,31 @@ nav__menu3.onclick = function () {
 nav__menu4.onclick = function () {
     toggle.classList.remove('activador');
     navbarx.classList.remove('activador');
+}
+
+//======MODO ISCURO
+// const btnSwitch = document.querySelector('#switch')
+
+// btnSwitch.addEventListener('click',()=>{
+btnSwitch.onclick = function () {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    //guardarmos el modo oscuro en localstorage.
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+
+    }else{
+        localStorage.setItem('dark-mode', 'false');
+    }
+
+};
+
+//obtenemos el modo actualizar
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+}else{
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
 }
