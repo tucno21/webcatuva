1d1210e01178b69556d0789fb465edda48dc78dc
tucno21 <carlitostucno@gmail.com>
2021-04-25 07:55:26 -0500

    toggle update

--- dev/js/main-menu.js
+++ dev/js/main-menu.js
@@ -1,7 +1,29 @@
-const toggleMenu = document.getElementById('menu-toggle');
-const topMenu = document.getElementById('top-menu');
+const toggle = document.getElementById('toggle');
+const navbarx = document.getElementById('navbarx');
 
+const nav__menu1 = document.getElementById('nav__menu1');
+const nav__menu2 = document.getElementById('nav__menu2');
+const nav__menu3 = document.getElementById('nav__menu3');
+const nav__menu4 = document.getElementById('nav__menu4');
 
-toggleMenu.addEventListener('click', () => {
-    topMenu.classList.toggle('activador');
-});
+toggle.onclick = function () {
+    toggle.classList.toggle('activador');
+    navbarx.classList.toggle('activador');
+}
+
+nav__menu1.onclick = function () {
+    toggle.classList.remove('activador');
+    navbarx.classList.remove('activador');
+}
+nav__menu2.onclick = function () {
+    toggle.classList.remove('activador');
+    navbarx.classList.remove('activador');
+}
+nav__menu3.onclick = function () {
+    toggle.classList.remove('activador');
+    navbarx.classList.remove('activador');
+}
+nav__menu4.onclick = function () {
+    toggle.classList.remove('activador');
+    navbarx.classList.remove('activador');
+}
