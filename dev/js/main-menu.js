14ce112f0eab3e96f22aaf3b7e58e604d5170ee6
tucno21 <carlitostucno@gmail.com>
2021-06-04 21:22:23 -0500

    animacion

--- dev/js/main-menu.js
+++ dev/js/main-menu.js
@@ -30,6 +30,8 @@ nav__menu4.onclick = function () {
     navbarx.classList.remove('activador');
 }
 
+
+
 //======MODO ISCURO
 // const btnSwitch = document.querySelector('#switch')
 
@@ -56,3 +58,33 @@ if(localStorage.getItem('dark-mode') === 'true'){
     document.body.classList.remove('dark');
     btnSwitch.classList.remove('active');
 }
+
+// // ANIMACION ============
+const sr = ScrollReveal();
+
+const animar = (clase, duracion, origen, distancia) => {
+    sr.reveal(clase, {
+        duration: duracion,
+        origin: origen,
+        distance: distancia,
+        reset:true
+    });
+}
+
+animar('.portada__title',2500,'top','20px');
+animar('.portada__paragraph',2500,'top','20px');
+animar('.portada__image',2500,'bottom','20px');
+
+animar('.presentacion__title',2500,'top','20px');
+animar('.presentacion__paragraph',2500,'top','20px');
+animar('.presentacion__image',2500,'bottom','20px');
+
+animar('.services__grandtitle',2500,'top','20px');
+animar('.services__box',2500,'left','40px');
+
+animar('.technology__title',2500,'top','20px');
+animar('.technology__paragraph',2500,'top','20px');
+animar('.technology__box',2500,'left','40px');
+
+
+
